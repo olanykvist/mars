e6eb19ce2d089b1917893d397a2eb59cd43e55cb
ola <ola@olanykvist.se>
2014-10-28 00:50:46 +0100

    Added Common project

---
+++ Common/NativeFunctions.cs
@@ -0,0 +1,11 @@
+﻿namespace MARS.Common
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    internal static class NativeFunctions
+    {
+        [DllImport("shell32")]
+        public static extern UInt32 SHGetKnownFolderPath(ref Guid id, int flags, IntPtr token, out IntPtr path);
+    }
+}
