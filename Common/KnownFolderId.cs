23c88736fbaf5b53a6839efbac0b03bbbb13cff8
Ola Nykvist <ola@olanykvist.se>
2014-11-05 10:46:34 +0100

    More work in Common

---
+++ Common/KnownFolderId.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="KnownFolderId.cs" company="Master Arms">
+//     Copyright (c) Master Arms. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace MARS.Common
+{
+    using System;
+
+    /// <summary>
+    /// This class contains Guids that identify standard folders registered with the system
+    /// </summary>
+    public class KnownFolderId
+    {
+        /// <summary>
+        /// Default path: %USERPROFILE%\Saved Games
+        /// </summary>
+        public static readonly Guid SavedGames = new Guid("{4C5C32FF-BB9D-43b0-B5B4-2D72E54EAAA4}");
+
+        /// <summary>
+        /// Default path: %LOCALAPPDATA% (%USERPROFILE%\AppData\Local)
+        /// </summary>
+        public static readonly Guid LocalAppData = new Guid("{F1B32785-6FBA-4FCF-9D55-7B8E7F157091}");
+    }
+}
--- Common/NativeFunctions.cs
+++ Common/NativeFunctions.cs
@@ -1,11 +1,27 @@
-﻿namespace MARS.Common
+﻿//-----------------------------------------------------------------------
+// <copyright file="NativeFunctions.cs" company="Master Arms">
+//     Copyright (c) Master Arms. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace MARS.Common
 {
     using System;
     using System.Runtime.InteropServices;
+    using System.Text;
 
+    /// <summary>
+    /// This class contains external Win32 API functions
+    /// </summary>
     public static class NativeFunctions
     {
-        [DllImport("shell32")]
-        public static extern UInt32 SHGetKnownFolderPath(ref Guid id, int flags, IntPtr token, out IntPtr path);
+        [DllImport("shell32.dll", CharSet = CharSet.Auto)]
+        internal static extern uint SHGetKnownFolderPath(ref Guid id, int flags, IntPtr token, out StringBuilder path);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        internal static extern uint GetPrivateProfileString(string appName, string keyName, string defaultValue, StringBuilder returnedString, uint size, string fileName);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        internal static extern bool WritePrivateProfileString(string appName, string keyName, string value, string fileName);
     }
 }
