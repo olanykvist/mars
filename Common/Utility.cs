42f5d5f1d0080e9551db079881a275b5654b8ba0
Ola Nykvist <ola@olanykvist.se>
2015-03-30 23:37:30 +0200

    Added StyleCop complient comments to Common

--- Common/Utility.cs
+++ Common/Utility.cs
@@ -28,14 +28,30 @@ namespace MARS.Common
             return builder.ToString();
         }
 
+        /// <summary>
+        /// Reads a configuration value as a string from configuration file
+        /// </summary>
+        /// <param name="appName">Section in configuration file</param>
+        /// <param name="keyName">Key name</param>
+        /// <param name="defaultValue">Default value</param>
+        /// <param name="fileName">Path to configuration file</param>
+        /// <returns>The value read from the file if successful, otherwise the default value provided</returns>
         public static string ReadConfigurationString(string appName, string keyName, string defaultValue, string fileName)
         {
             const int SIZE = 255;
-            StringBuilder builder = new StringBuilder(SIZE);
+            var builder = new StringBuilder(SIZE);
             NativeFunctions.GetPrivateProfileString(appName, keyName, defaultValue, builder, SIZE, fileName);
             return builder.ToString();
         }
 
+        /// <summary>
+        /// Reads a configuration value as an integer from an configuration file
+        /// </summary>
+        /// <param name="appName">Section in configuration file</param>
+        /// <param name="keyName">Key name</param>
+        /// <param name="defaultValue">Default value</param>
+        /// <param name="fileName">Path to configuration file</param>
+        /// <returns>The value read from the file if successful, otherwise the default value provided</returns>
         public static int ReadConfigurationInteger(string appName, string keyName, int defaultValue, string fileName)
         {
             var temp = Utility.ReadConfigurationString(appName, keyName, defaultValue.ToString(), fileName);
@@ -45,18 +61,31 @@ namespace MARS.Common
             {
                 value = Convert.ToInt32(temp);
             }
-            catch (Exception)
+            catch (FormatException)
             {
             }
 
             return value;
         }
 
+        /// <summary>
+        /// Writes a configuration string to a configuration file
+        /// </summary>
+        /// <param name="appName">Section in configuration file</param>
+        /// <param name="keyName">Key name</param>
+        /// <param name="value">Value to set </param>
+        /// <param name="fileName">Path to configuration file</param>
         public static void WriteConfigurationString(string appName, string keyName, string value, string fileName)
         {
             NativeFunctions.WritePrivateProfileString(appName, keyName, value, fileName);
         }
 
+        /// <summary>
+        /// Compares if two files are equal.
+        /// </summary>
+        /// <param name="firstFile">First file</param>
+        /// <param name="secondFile">Second file</param>
+        /// <returns>True if the files are equal, otherwise false</returns>
         public static bool FilesAreEqual(FileInfo firstFile, FileInfo secondFile)
         {
             if (!firstFile.Exists || !secondFile.Exists)
